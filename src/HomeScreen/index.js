60c5d43264b03b6d03c1805135247e3c8ba68bf1
pipopotamasu <ec0156hx@gmail.com>
2018-04-20 00:01:36 +0900

    profile

--- src/HomeScreen/index.js
+++ src/HomeScreen/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import HomeScreen from "./HomeScreen.js";
 import MainScreenNavigator from "../ChatScreen/index.js";
-// import Profile from "../ProfileScreen/index.js";
+import Profile from "../ProfileScreen/index.js";
 import SideBar from "../SideBar/SideBar.js";
 import { DrawerNavigator } from "react-navigation";
 const HomeScreenRouter = DrawerNavigator(
   {
     Home: { screen: HomeScreen },
     Chat: { screen: MainScreenNavigator },
-    // Profile: { screen: Profile }
+    Profile: { screen: Profile }
   },
   {
     contentComponent: props => <SideBar {...props} />
